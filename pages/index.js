fec05cb7162bd227bfec68433212b799084df962
Andy Rossi <andrewrocco@users.noreply.github.com>
2022-05-01 14:17:36 -0400

    we have data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
-export default function Index() {
+import { getAllPostsForHome } from "../lib/api";
+
+export default function Index({ preview, allPosts }) {
+  const featuredPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+
   return (
-    <div>Hello</div>
+    <>
+    {featuredPost && (
+      <h2>{featuredPost.title}</h2>
+    )}
+    </>
   )
+}
+
+export async function getStaticProps({ preview = false }) {
+  const allPosts = (await getAllPostsForHome(preview)) ?? []
+  return {
+    props: { preview, allPosts },
+  };
 }
