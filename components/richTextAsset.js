7369423234b74cee7e1813b18dc8acf5897e0590
Andy Rossi <andrewrocco@users.noreply.github.com>
2022-05-01 16:03:41 -0400

    more tweaks

--- components/richTextAsset.js
+++ components/richTextAsset.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 
+const contentfulLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 65}&fm=webp`
+}
+
 export default function RichTextAsset({ id, assets }) {
   const asset = assets?.find((asset) => asset.sys.id === id)
 
   if (asset?.url) {
-    return <Image src={asset.url} layout="fill" alt={asset.description} />
+    return <Image src={asset.url} width={asset.width} height={asset.height} alt={asset.description} loader={contentfulLoader} />
   }
 
   return null
