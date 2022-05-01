321171c7f87a8161cbb2fd4b66f5216b87085b69
Andy Rossi <andrewrocco@users.noreply.github.com>
2022-05-01 15:37:35 -0400

    adding post body styles

--- components/postBody.js
+++ components/postBody.js
@@ -1,12 +1,26 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import { BLOCKS } from '@contentful/rich-text-types'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
+import markdownStyles from './markdown-styles.module.css';
+import RichTextAsset from './richTextAsset';
+
+const customMarkdownOptions = (content) => ({
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => (
+      <RichTextAsset
+        id={node.data.target.sys.id}
+        assets={content.links.assets.block}
+      />
+    ),
+  },
+})
 
 export default function PostBody({ body }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <div>
+      <div className={markdownStyles['markdown']}>
         {documentToReactComponents(
-          body.json
+          body.json,
+          customMarkdownOptions(body)
         )}
       </div>
     </div>
