71eb957ba9b4e452fc270904691439a82ce0f710
Andy Rossi <andrewrocco@users.noreply.github.com>
2022-05-01 15:23:36 -0400

    getting post data

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,45 @@
-export default function Post() {
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import ErrorPage from 'next/error';
+
+import Container from '../../components/container';
+import Layout from '../../components/layout';
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api';
+
+export default function Post({ post, morePost, preview }) {
+  const router = useRouter();
+
+  if(!router.isFallback && !post) {
+    return <ErrorPage statusCode={404} />
+  }
+
   return (
-    <div>
-      <h1>This is a post!</h1>
-    </div>
+    <Layout preview={preview}>
+      <Container>
+        {router.isFallback ? (
+          <h1>Loading...</h1>
+        ) : (<div>Hello</div>)}
+      </Container>
+    </Layout>
   );
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getPostAndMorePosts(params.slug, preview);
+
+  return {
+    props: {
+      preview,
+      post: data?.post ?? null,
+      morePosts: data?.morePosts ?? null,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const allPosts = await getAllPostsWithSlug();
+  return {
+    paths: allPosts?.map(({ slug }) => `/posts/${slug}`) ?? [],
+    fallback: true,
+  }
 }
